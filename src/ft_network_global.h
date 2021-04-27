774b087b4ff9c8b7a494fc39fdc1d2e1cb1bee10
dzonda <dzonda@student.42.fr>
2021-04-27 13:01:58 +0200

    refactor: network global h

--- src/ft_network_global.h
+++ src/ft_network_global.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_network_global.h                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dzonda <dzonda@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 22:14:00 by dzonda            #+#    #+#             */
-/*   Updated: 2021/03/23 21:45:02 by dzonda           ###   ########lyon.fr   */
+/*   Updated: 2021/04/23 14:43:39 by user42           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,37 @@
 # define FT_NETWORK_GLOBAL_H
 
 # include "../libft/libft.h"
+
+# include <stdio.h>
+# include <string.h>
+
+# include <sys/socket.h>
+# include <sys/time.h>
+# include <sys/types.h>
+
+# include <time.h>
+# include <arpa/inet.h>
+# include <netinet/in.h>
+# include <errno.h> 
+# include <netdb.h>
+
+# include <netinet/ip.h>
+# include <netinet/udp.h>
+# include <netinet/ip_icmp.h>
+
+# define INVALID_SOCKET -1
+# define SOCKET_ERROR -1
+# define closesocket(s) close(s)
+
+typedef int                 t_socket;
+typedef struct sockaddr_in  t_sockaddr_in;
+typedef struct sockaddr     t_sockaddr;
+typedef struct in_addr      t_in_addr;
+typedef struct addrinfo     t_addrinfo;
+typedef struct ip           t_ip;
+typedef struct udphdr       t_udp;
+typedef struct icmp         t_icmp;
+
+typedef struct timeval t_timeval;
 
 #endif
