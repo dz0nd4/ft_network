0082ad2896014a5a5b384dcb835c819008d87d48
dzonda <dzonda@student.42.fr>
2021-05-10 15:35:29 +0200

    update: simplify dispatch network

--- src/network/ft_network.c
+++ src/network/ft_network.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_network.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dzonda <dzonda@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 22:12:50 by dzonda            #+#    #+#             */
-/*   Updated: 2021/03/23 19:08:33 by dzonda           ###   ########lyon.fr   */
+/*   Updated: 2021/05/10 00:58:40 by user42           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int			main(int argc, const char *argv[])
 {
-  static const t_net net_command[FT_NET_MAX] = {
-    { "./ft_traceroute", ft_traceroute },
-    { "./ft_nmap", ft_nmap }
-  };
-  int key;
+  // static const t_net net_command[FT_NET_MAX] = {
+  //   { "./ft_traceroute", ft_traceroute },
+  //   { "./ft_nmap", ft_nmap }
+  // };
+  // int key;
 
-  key = -1;
-  while (++key < FT_NET_MAX)
-    if (ft_strequal(argv[0], net_command[key].name))
-      return (net_command[key].dist(argc, argv));
-	return (EXIT_FAILURE);
+  // key = -1;
+  // while (++key < FT_NET_MAX)
+  //   if (ft_strequal(argv[0], net_command[key].name))
+  //     return (net_command[key].dist(argc, argv));
+	return (ft_traceroute(argc, argv));
 }
