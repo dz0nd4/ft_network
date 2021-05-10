5b4f656b04cd16b964c0283a230325e089941096
dzonda <dzonda@student.42.fr>
2021-05-10 18:24:16 +0200

    add: ft_isdigitstr

--- libft/src/is/ft_isdigitstr.c
+++ libft/src/is/ft_isdigitstr.c
@@ -1,33 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is.h                                            :+:      :+:    :+:   */
+/*   ft_isdigitstr.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user42 <user42@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/13 03:43:56 by dzonda            #+#    #+#             */
-/*   Updated: 2021/05/10 17:09:53 by user42           ###   ########lyon.fr   */
+/*   Created: 2017/12/12 20:09:49 by dzonda            #+#    #+#             */
+/*   Updated: 2021/05/10 17:09:46 by user42           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_IS_H
-# define FT_IS_H
+#include "ft_is.h"
 
-# include <sys/stat.h>
-# include "../mem/ft_mem.h"
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_islower(int c);
-int		ft_isupper(int c);
-int		ft_isblank(int c);
-int		ft_isspace(int c);
-int		ft_isdir(char *arg);
-int		ft_isreg(char *arg);
-
-int		ft_isdigitstr(char *str);
-
-#endif
+int		ft_isdigitstr(char *str)
+{
+  while (*str) {
+    if (!ft_isdigit(*str))
+      return (0);
+    str++;
+ }
+	return (1);
+}
